8373627b8fe5cff905843bfc1c799de3ce27cc16
GiovannaBlanco <giovanna.b.santos9@aluno.senai.br>
2025-09-16 09:08:14 -0300

    :white_check_mark: Adicionando teste Qual a DATABASE

--- backend/src/prisma/index.ts
+++ backend/src/prisma/index.ts
@@ -2,4 +2,5 @@ import { PrismaClient } from "../generated/prisma/client";
 
 const prismaClient = new PrismaClient()
 
+console.log('DATABASE_URL que aplicação está usando: ', process.env.DATABASE_URL)
 export default prismaClient
